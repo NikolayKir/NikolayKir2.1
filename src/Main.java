15e9cd0b946abf9e4bf8bcb9f8103d1fe68b390d
NikolayKir <jahberith@gmail.com>
2022-06-24 18:00:27 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int Cost = 220;
+        int Bonus = 20;
+        int sum = 0;
+
+        sum = Cost / Bonus;
+        System.out.println(sum);
+    }
+}
